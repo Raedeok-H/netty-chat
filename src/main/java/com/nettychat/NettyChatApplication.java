82046c1b512f0ea845c7b1fa83018a4062bbe21d
Raedeok-H <raedeokhwang@gmail.com>
2024-04-29 17:17:51 +0900

    init

---
+++ src/main/java/com/nettychat/NettyChatApplication.java
@@ -0,0 +1,14 @@
+package com.nettychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyChatApplication.class, args);
+    }
+}
+
+
